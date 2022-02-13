8a5be31b9b864d4706d6eedba021cd9433f0a6b6
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-02-12 16:36:04 -0800

    Update main.js
    hooray

--- main.js
+++ main.js
@@ -28,3 +28,56 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speach_data1 + speach_data2);
     synth.speak(utterThis)
 }
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult()
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+        if(results[0].label == "Happy")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128522; &#128515;";
+        }
+        if(results[0].label == "Sad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128557; &#128555;";
+        }
+        if(results[0].label == "Angry")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128545;";
+        }
+        if(results[0].label == "Emotionless")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128528;";
+        }
+        if(results[1].label == "Happy")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128522; &#128515;";
+        }
+        if(results[1].label == "Sad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128557; &#128555;";
+        }
+        if(results[1].label == "Angry")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128545;";
+        }
+        if(results[1].label == "Emotionless")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128528;";
+        }
+    }
+}
